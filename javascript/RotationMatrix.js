81f33be0dfc4d88ca4f68c7d753eb9dd1ab1d4d5
Robert <2085628921@qq.com>
2020-04-07 16:58:09 +0800

    fix

--- javascript/RotationMatrix.js
+++ javascript/RotationMatrix.js
@@ -1,24 +1,24 @@
 /**
  * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
- * 
+ *
  * 不占用额外内存空间能否做到？
- * 
+ *
  * 示例 1:
- * 
- * 给定 matrix = 
+ *
+ * 给定 matrix =
  * [
  *   [1,2,3],
  *   [4,5,6],
  *   [7,8,9]
  * ],
- * 
+ *
  * 原地旋转输入矩阵，使其变为:
  * [
  *   [7,4,1],
  *   [8,5,2],
  *   [9,6,3]
  * ]
- * 
+ *
  * 题目链接: https://leetcode-cn.com/problems/rotate-matrix-lcci/submissions/
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
@@ -29,9 +29,12 @@ const app = matrix => {
       ;[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
     }
   }
-  matrix.map(item => {
-    return item.reverse()
-  })
+  //   matrix.map(item => {
+  //     return item.reverse()
+  //   })
+  for (let a of matrix) {
+    a.reverse()
+  }
   return matrix
 }
 console.log(
